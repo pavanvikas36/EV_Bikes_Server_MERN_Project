8aa17175f9fde5b0453cbd87d5f5f7100e50c31e
pavanvikas36 <pavanvivek36@gmail.com>
2025-09-16 14:54:50 +0530

    Mern Stack Project

--- Controllers/buyerController.js
+++ Controllers/buyerController.js
@@ -1,7 +1,33 @@
-exports.viewAllVehicles = (req, res, next) => {
-    res.send("All Vehicles")
+const VehicleModel = require("../Models/vehicleModel.js")
+
+exports.viewAllVehicles = async (req, res, next) => {
+    try {
+        const vechiles = await VehicleModel.find()
+        return res.json({message: "All Vehicles", data: vechiles})
+    } catch (error) {
+        return res.status(500).json({
+            message: "All vehicles server error",
+            error: error.message
+        })
+    }
+    // res.send("All Vehicles")
 }
 
-exports.viewVehiclesById = (req, res, next) => {
-    res.send("Vehicle Details")
+exports.viewVehiclesById = async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const vehicle = await VehicleModel.findById(id)
+
+        if(!vehicle){
+            return res.status(404).json({message: "Vehicle Not Found"})
+        }
+
+        return res.json({message: "Vechile Detils", data: vehicle})
+    } catch (error) {
+        return res.status(500).json({
+            message: "Vechile Details Server Error",
+            error: error.message
+        })
+    }
+    // res.send("Vehicle Details")
 }
