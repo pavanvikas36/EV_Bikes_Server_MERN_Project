10995bd2483f913b627d0f1b1388abe9321cef02
pavanvikas36 <pavanvivek36@gmail.com>
2025-09-17 16:21:11 +0530

    Mern Stack Project

--- Controllers/buyerController.js
+++ Controllers/buyerController.js
@@ -13,9 +13,44 @@ exports.viewAllVehicles = async (req, res, next) => {
     // res.send("All Vehicles")
 }
 
+// exports.viewVehiclesById = async (req, res, next) => {
+//     try {
+//         const { id } = req.params;
+//         const vehicle = await VehicleModel.findById(id);
+
+//         if (!vehicle) {
+//             return res.status(404).json({ message: "Vehicle Not Found" });
+//         }
+
+//         return res.json({
+//             message: "Vehicle Details",
+//             data: vehicle
+//         });
+//     } catch (error) {
+//         return res.status(500).json({
+//             message: "Vehicle Details Server Error",
+//             error: error.message
+//         });
+//     }
+// };
+
+
+
+// Corrected viewVehiclesById function
 exports.viewVehiclesById = async (req, res, next) => {
     try {
         const { id } = req.params;
+        
+        // Check if ID is provided
+        if (!id) {
+            return res.status(400).json({ message: "Vehicle ID is required" });
+        }
+        
+        // Check if ID is a valid MongoDB ObjectId
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid vehicle ID format" });
+        }
+
         const vehicle = await VehicleModel.findById(id);
 
         if (!vehicle) {
@@ -23,12 +58,13 @@ exports.viewVehiclesById = async (req, res, next) => {
         }
 
         return res.json({
-            message: "Vehicle Details",
+            message: "Vehicle Details Retrieved Successfully",
             data: vehicle
         });
     } catch (error) {
+        console.error("Vehicle Details Server Error:", error);
         return res.status(500).json({
-            message: "Vehicle Details Server Error",
+            message: "Server Error While Fetching Vehicle Details",
             error: error.message
         });
     }
